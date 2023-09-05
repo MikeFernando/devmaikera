471f9f1726e2434e147386912733172bcc6c2d23
mikera <mike.silva@pixter.com.br>
2023-09-05 15:32:44 -0300

    remove pb footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className="border-t-[1px] bg-gray-50 px-5 py-10 text-zinc-500">
-      <div className="mx-auto mb-16 flex max-w-7xl justify-between text-sm">
+      <div className="mx-auto flex max-w-7xl justify-between text-sm">
         <p className="">2023© All Rights Reserved.</p>
         <div className="flex gap-4">
           <Instagram />
