9b27cb552dcaa60edeec598dd3899bbc5fa9cd6e
mikera <mike.silva@pixter.com.br>
2023-09-11 14:23:17 -0300

    change photos projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,10 +4,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { ArrowLeft } from 'lucide-react'
 
-import spiderman from '@/assets/projects/spiderman.gif'
-import wordtrip from '@/assets/projects/wordtrip.gif'
-import bikecraft from '@/assets/projects/bikecraft.gif'
-import apple from '@/assets/projects/apple.gif'
+import spiderman from '@/assets/slider/spiderman-capa.png'
+import wordtrip from '@/assets/slider/wortrip-capa.png'
+import bikecraft from '@/assets/slider/bikecraft-capa.png'
+import apple from '@/assets/slider/apple-capa.png'
+
+// import { Loading } from '@/components/Loading'
 
 export default function Projects() {
   return (
@@ -21,40 +23,70 @@ export default function Projects() {
             Projects
           </h1>
         </div>
-        <ul className="grid grid-cols-1 gap-10 p-5 md:grid-cols-2">
-          <li>
-            <Link
-              href="https://mikefernando-spiderman.netlify.app/"
-              target="_blank"
-            >
-              <Image src={spiderman} alt="spiderman" className="rounded-md" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://worldtrip.vercel.app/continent/europe"
-              target="_blank"
-            >
-              <Image src={wordtrip} alt="spiderman" className="rounded-md" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://mikefernando-bikecraft.netlify.app/"
-              target="_blank"
-            >
-              <Image src={bikecraft} alt="spiderman" className="rounded-md" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://mikefernando-apple.netlify.app/"
-              target="_blank"
-            >
-              <Image src={apple} alt="spiderman" className="rounded-md" />
-            </Link>
-          </li>
-        </ul>
+        <div className="mt-10 flex items-center justify-center">
+          <ul className="grid grid-cols-1 gap-10 p-5 md:grid-cols-2">
+            <li className="rounded-lg bg-gray-100 p-6 dark:bg-cinza-900">
+              <Link
+                href="https://mikefernando-spiderman.netlify.app/"
+                target="_blank"
+              >
+                <Image
+                  width={500}
+                  height={500}
+                  priority
+                  src={spiderman}
+                  alt="spiderman"
+                  className="rounded-md"
+                />
+              </Link>
+            </li>
+            <li className="rounded-lg bg-gray-100 p-6 dark:bg-cinza-900">
+              <Link
+                href="https://worldtrip.vercel.app/continent/europe"
+                target="_blank"
+              >
+                <Image
+                  width={500}
+                  height={500}
+                  priority
+                  src={wordtrip}
+                  alt="spiderman"
+                  className="rounded-md"
+                />
+              </Link>
+            </li>
+            <li className="rounded-lg bg-gray-100 p-6 dark:bg-cinza-900">
+              <Link
+                href="https://mikefernando-bikecraft.netlify.app/"
+                target="_blank"
+              >
+                <Image
+                  width={500}
+                  height={500}
+                  priority
+                  src={bikecraft}
+                  alt="spiderman"
+                  className="rounded-md"
+                />
+              </Link>
+            </li>
+            <li className="rounded-lg bg-gray-100 p-6 dark:bg-cinza-900">
+              <Link
+                href="https://mikefernando-apple.netlify.app/"
+                target="_blank"
+              >
+                <Image
+                  width={500}
+                  height={500}
+                  priority
+                  src={apple}
+                  alt="spiderman"
+                  className="rounded-md"
+                />
+              </Link>
+            </li>
+          </ul>
+        </div>
       </main>
     </div>
   )
